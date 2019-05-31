7e24c148b3dbe33fcdbf9226d016e314b01301b3
Belinda Liu <belinda.liu@rocketmail.com>
2019-05-31 11:55:06 -0400

    add color and form js

--- assignment4.js
+++ assignment4.js
@@ -1,1 +1,7 @@
 // javascript file
+
+// get values from form when submit is clicked
+document.getElementById("submit_button").onclick = function(){create_table()};
+
+// function to create table here
+
