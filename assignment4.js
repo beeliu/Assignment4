bd013e7750471ec06ee6d2a60d2ad7a7edcbbdb2
Erica Elliston <erica.elliston06@myhunter.cuny.edu>
2019-06-02 00:01:17 -0400

    functions to add/delete rows/column closes issue#1,#2

--- assignment4.js
+++ assignment4.js
@@ -1,7 +1,121 @@
 // javascript file
 
-// get values from form when submit is clicked
-document.getElementById("submit_button").onclick = function(){create_table()};
+/* the following function create table on Submit see event listener below functions */
 
-// function to create table here
+/*function to add rows and columns */
 
+/*function to add rows */
+function addRows(table,row){
+  for(let i = 0; i<row; i++){
+     let trow = table.insertRow(i);
+     for(let j =0; j< table.rows[1].cells.length; j++){
+            trow.insertCell(j);
+    }
+}
+}
+
+/*function to add columns */
+function addCols(table,column){
+  for(let i=0; i<column; i++){
+    table.rows[0].insertCell(0);
+    for(let j = 1; j< table.rows.length; j++){
+          table.rows[j].insertCell(0);
+    }
+  }
+}
+
+/*function to add rows and columns when both are > 0 */
+function addRowsCol(table, row, column){
+  for(let i = 0; i<row; i++){
+    trow = table.insertRow(i);
+    for(let j = 0; j<column; j++){
+      trow.insertCell(j);
+    }
+  }
+}
+
+/*function to delete rows */
+function deleteRows(table,row){
+  /*row should be a negative number*/
+  for(let i = row; i< 0; i++){
+    table.deleteRow(0);
+  }
+}
+
+/*function to delete columns*/
+function deleteCols(table, column){
+  for(let i = 0; i< table.rows.length; i ++){
+    /*column should be a negative number*/
+    for(let j = column; j< 0; j++){
+      table.rows[i].deleteCell(j);
+    }
+  }
+}
+
+/* event handler function*/
+function modifyGrid(event){
+
+  event.preventDefault();
+  let table = document.getElementById('Table');
+
+/*extracts values from form*/
+  let row = document.getElementById('row').value;
+  let column = document.getElementById('column').value;
+
+/*how many rows/columns to delete/add */
+  row = row - table.rows.length;
+  column = column - table.rows[0].cells.length;
+
+ console.log( row + " "+ column);
+
+
+/*logic of evaluations */
+if ( row > 0 && column > 0){
+    addRowsCol(table,row, column);
+
+}
+
+else if (row < 0 && column < 0){
+  /* delete cells */
+    deleteRow(table,row);
+    deleteCols(table,column);
+}
+
+else if ( row > 0 && column < 0){
+  /* add rows, delete cells */
+      deleteCols(table,column);
+      addRows(table,row);
+}
+
+else if (row < 0 && column > 0){
+  /*add cells, delete rows*/
+  deleteRows(table,row);
+  addCols(table,column);
+}
+else if (row === 0 && column > 0){
+  /*leave rows as is add columns*/
+     addCols(table,column);
+}
+else if (row === 0 && column < 0){
+  /*leave rows delete columns */
+  deleteCols(table,column);
+}
+else if (column === 0 && row > 0){
+  /*leave columns add rows*/
+   addRows(table,row);
+}
+else if (column === 0 && row < 0){
+  /*leave columns delete rows */
+  deleteRows(table,row);
+}
+else{
+  console.log("User has not changed the grid");
+
+ }
+
+}
+
+
+
+let submission = document.getElementById('Form');
+submission.addEventListener("submit",modifyGrid);
